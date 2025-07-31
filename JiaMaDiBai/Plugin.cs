260f5a518fabc84413cd5f278ec18e6d19585b38
xinya28 <mouzie@qq.com>
2025-07-31 13:09:04 +0800

    初步完成设置页面的注册(虽然什么也没写)

--- JiaMaDiBai/Plugin.cs
+++ JiaMaDiBai/Plugin.cs
@@ -5,18 +5,39 @@ using ClassIsland.Core.Controls.CommonDialog;//日志?
 using ClassIsland.Core.Extensions.Registry;//什么时候冒出来的，看不懂思密达
 using Microsoft.Extensions.DependencyInjection;//看不懂思密达
 using Microsoft.Extensions.Hosting;//看不懂思密达
+using System.IO;//看不懂
+using ClassIsland.Shared.Helpers;//看不懂
+//using JiaMaDiBai.Models;
+using JiaMaDiBai.Views.SettingsPages;//设置界面引用，名字好乱，别瞎改
+using YamlDotNet.Serialization;//看不懂
 
 namespace JiaMaDiBai//感觉这个名字不太好用,但是也不易重名吧
 {
     [PluginEntrance]//入口在这?
     public class Plugin : PluginBase
     {
+        public Settings Settings { get; set; } = new();
+        
+        
         public override void Initialize(HostBuilderContext context, IServiceCollection services)
         {
-            //services.AddSettingsPage<JiaMaDiBaiSettingPage>();//设置界面还没写，别急
+
+
+            /*
+            Settings = ConfigureFileHelper.LoadConfig<Settings>(Path.Combine(PluginConfigFolder, "Settings.json"));  // 加载配置文件
+            Settings.PropertyChanged += (sender, args) =>
+            {
+                ConfigureFileHelper.SaveConfig<Settings>(Path.Combine(PluginConfigFolder, "Settings.json"), Settings);  // 保存配置文件
+            };
+            //读取和保存配置文件
+            */
+
+
+            services.AddSettingsPage<MainPage>();//设置界面
             //services.AddSingleton<JiaMaDiBai_Ser>();//服务注册,没写，别急
             //sservices.AddComponent<JiaMaDiBai_Compnt>();//组件注册，没写，别急
             CommonDialog.ShowInfo("加码迪拜!!!启动成功!");
         }//插件初始化,要在这里完成相关服务的注册，不然有你好果子吃
+        //public override void OnShutdown() { }
     }
 }
---
+++ JiaMaDiBai/SettingsPages/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ClassIsland.Core.Abstractions.Controls;
+using ClassIsland.Core.Attributes;
+using ClassIsland.Core.Enums.SettingsWindow;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JiaMaDiBai.Views.SettingsPages;
+
+/// <summary>
+/// SettingsPage1.xaml 的交互逻辑
+/// </summary>
+
+[SettingsPageInfo(
+    "JMDB.MainPage",
+    "加码迪拜的主设置页面",
+    PackIconKind.CogOutline,
+    PackIconKind.Cog,
+    SettingsPageCategory.External
+)]
+public partial class MainPage : SettingsPageBase
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
